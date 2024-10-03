c85b23979cef1f631dfb18b99f384a3b8d02c22f
Suraj Deshmukh <surajd.service@gmail.com>
2024-10-03 13:35:38 -0700

    check-prereqs.sh: Add instructions to install tools
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- check-prereqs.sh
+++ check-prereqs.sh
@@ -14,10 +14,27 @@ function check_cli() {
     for CLI in "${CLIS[@]}"; do
         if ! command -v "${CLI}" &>/dev/null; then
             error "The CLI '${CLI}' is not installed. Please install it and try again."
+
+            # Add instructions to install tools
+            case "${CLI}" in
+            "az")
+                error "To install Azure CLI (az) follow instructions here: https://learn.microsoft.com/en-us/cli/azure/install-azure-cli"
+                ;;
+            "kustomize")
+                error "To install Kustomize follow instructions here: https://kubectl.docs.kubernetes.io/installation/kustomize/"
+                ;;
+            "kubectl")
+                error "To install kubectl follow instructions here: https://kubernetes.io/docs/tasks/tools/#kubectl"
+                ;;
+            "skopeo")
+                error "To install Skopeo follow instructions here: https://github.com/containers/skopeo/blob/main/install.md"
+                ;;
+            esac
+
             exit 1
         fi
     done
 }
 
 # Check if the required CLIs are installed
-check_cli "curl" "python3" "az" "kustomize" "kubectl" "git" "openssl" "skopeo"
+check_cli "curl" "python3" "git" "openssl" "az" "kustomize" "kubectl" "skopeo"
